335a006eeacaaec178986b39b0eae8877f087639
ayy123 <qy1996@hotmail.com>
2024-09-15 18:54:52 +0800

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -37,125 +37,109 @@ async function handleRequest(request) {
       // If no rules are selected, use the default rules
       const rulesToUse = selectedRules.length > 0 ? selectedRules : ['广告拦截', '谷歌服务', '国外媒体', '电报消息'];
 
-      const xrayUrl = `${url.origin}/xray?config=${encodeURIComponent(inputString)}`;
-      const singboxUrl = `${url.origin}/singbox?config=${encodeURIComponent(inputString)}&selectedRules=${encodeURIComponent(JSON.stringify(rulesToUse))}&customRules=${encodeURIComponent(JSON.stringify(customRules))}`;
-      const clashUrl = `${url.origin}/clash?config=${encodeURIComponent(inputString)}&selectedRules=${encodeURIComponent(JSON.stringify(rulesToUse))}&customRules=${encodeURIComponent(JSON.stringify(customRules))}`;
+      const xrayUrl = `${url.origin}/sub?target=xray&url=${encodeURIComponent(inputString)}`;
+      const singboxUrl = `${url.origin}/sub?target=singbox&url=${encodeURIComponent(inputString)}&selectedRules=${encodeURIComponent(JSON.stringify(rulesToUse))}&customRules=${encodeURIComponent(JSON.stringify(customRules))}`;
+      const clashUrl = `${url.origin}/sub?target=clash&url=${encodeURIComponent(inputString)}&selectedRules=${encodeURIComponent(JSON.stringify(rulesToUse))}&customRules=${encodeURIComponent(JSON.stringify(customRules))}`;
 
       return new Response(generateHtml(xrayUrl, singboxUrl, clashUrl), {
         headers: { 'Content-Type': 'text/html' }
       });
-    } else if (url.pathname.startsWith('/singbox') || url.pathname.startsWith('/clash')) {
-      const inputString = url.searchParams.get('config');
-      let selectedRules = url.searchParams.get('selectedRules');
-      let customRules = url.searchParams.get('customRules');
+    } else if (url.pathname.startsWith('/sub')){
 
-      if (!inputString) {
-        return new Response('Missing config parameter', { status: 400 });
-      }
+      const target = url.searchParams.has('target') && url.searchParams.get('target') !== '' ? url.searchParams.get('target') : null;
 
-      // Deal with predefined rules
-      if (PREDEFINED_RULE_SETS[selectedRules]) {
-        selectedRules = PREDEFINED_RULE_SETS[selectedRules];
-      } else {
-        try {
-          selectedRules = JSON.parse(decodeURIComponent(selectedRules));
-        } catch (error) {
-          console.error('Error parsing selectedRules:', error);
-          selectedRules = PREDEFINED_RULE_SETS.minimal;
+      if (target === 'singbox' || target === 'clash') {
+        const inputString = url.searchParams.get('url');
+        let selectedRules = url.searchParams.get('selectedRules');
+        let customRules = url.searchParams.get('customRules');
+  
+        if (!inputString) {
+          return new Response('Missing config parameter', { status: 400 });
         }
-      }
-
-      // Deal with custom rules
-      try {
-        customRules = JSON.parse(decodeURIComponent(customRules));
-      } catch (error) {
-        console.error('Error parsing customRules:', error);
-        customRules = [];
-      }
-
-      let configBuilder;
-      if (url.pathname.startsWith('/singbox')) {
-        configBuilder = new ConfigBuilder(inputString, selectedRules, customRules);
-      } else {
-        configBuilder = new ClashConfigBuilder(inputString, selectedRules, customRules);
-      }
-
-      const config = await configBuilder.build();
-
-      return new Response(
-        url.pathname.startsWith('/singbox') ? JSON.stringify(config, null, 2) : config,
-        {
-          headers: {
-            'content-type': url.pathname.startsWith('/singbox')
-              ? 'application/json; charset=utf-8'
-              : 'text/yaml; charset=utf-8'
-          }
-        }
-      );
-
-    } else if (url.pathname === '/shorten') {
-      const originalUrl = url.searchParams.get('url');
-      if (!originalUrl) {
-        return new Response('Missing URL parameter', { status: 400 });
-      }
-
-      const shortCode = GenerateWebPath();
-      await SUBLINK_KV.put(shortCode, originalUrl);
-
-      const shortUrl = `${url.origin}/s/${shortCode}`;
-      return new Response(JSON.stringify({ shortUrl }), {
-        headers: { 'Content-Type': 'application/json' }
-      });
-    } else if (url.pathname.startsWith('/s/')) {
-      const shortCode = url.pathname.split('/')[2];
-      const originalUrl = await SUBLINK_KV.get(shortCode);
-
-      if (originalUrl === null) {
-        return new Response('Short URL not found', { status: 404 });
-      }
-
-      return Response.redirect(originalUrl, 302);
-    } else if (url.pathname.startsWith('/xray')) {
-      // Handle Xray config requests
-      const inputString = url.searchParams.get('config');
-      const proxylist = inputString.split('\n');
-
-      const finalProxyList = [];
-
-      for (const proxy of proxylist) {
-        console.log(proxy);
-        if (proxy.startsWith('http://') || proxy.startsWith('https://')) {
-          try {
-            const response = await fetch(proxy)
-            const text = await response.text();
-            let decodedText;
-            decodedText = decodeBase64(text.trim());
-            // Check if the decoded text needs URL decoding
-            if (decodedText.includes('%')) {
-              decodedText = decodeURIComponent(decodedText);
-            }
-            finalProxyList.push(...decodedText.split('\n'));
-          } catch (e) {
-            console.warn('Failed to fetch the proxy:', e);
-          }
+  
+        // Deal with predefined rules
+        if (PREDEFINED_RULE_SETS[selectedRules]) {
+          selectedRules = PREDEFINED_RULE_SETS[selectedRules];
         } else {
-          finalProxyList.push(proxy);
+          try {
+            selectedRules = JSON.parse(decodeURIComponent(selectedRules));
+          } catch (error) {
+            console.error('Error parsing selectedRules:', error);
+            selectedRules = PREDEFINED_RULE_SETS.minimal;
+          }
         }
+  
+        // Deal with custom rules
+        try {
+          customRules = JSON.parse(decodeURIComponent(customRules));
+        } catch (error) {
+          console.error('Error parsing customRules:', error);
+          customRules = [];
+        }
+  
+        let configBuilder;
+        if (target === 'singbox') {
+          configBuilder = new ConfigBuilder(inputString, selectedRules, customRules);
+        } else {
+          configBuilder = new ClashConfigBuilder(inputString, selectedRules, customRules);
+        }
+  
+        const config = await configBuilder.build();
+  
+        return new Response(
+          target === 'singbox' ? JSON.stringify(config, null, 2) : config,
+          {
+            headers: {
+              'content-type': target === 'singbox'
+                ? 'application/json; charset=utf-8'
+                : 'text/yaml; charset=utf-8'
+            }
+          }
+        );
+  
+      } else if (target === 'xray') {
+        // Handle Xray config requests
+        const inputString = url.searchParams.get('url');
+        const proxylist = inputString.split('\n');
+  
+        const finalProxyList = [];
+  
+        for (const proxy of proxylist) {
+          console.log(proxy);
+          if (proxy.startsWith('http://') || proxy.startsWith('https://')) {
+            try {
+              const response = await fetch(proxy)
+              const text = await response.text();
+              let decodedText;
+              decodedText = decodeBase64(text.trim());
+              // Check if the decoded text needs URL decoding
+              if (decodedText.includes('%')) {
+                decodedText = decodeURIComponent(decodedText);
+              }
+              finalProxyList.push(...decodedText.split('\n'));
+            } catch (e) {
+              console.warn('Failed to fetch the proxy:', e);
+            }
+          } else {
+            finalProxyList.push(proxy);
+          }
+        }
+  
+        const finalString = finalProxyList.join('\n');
+  
+        if (!finalString) {
+          return new Response('Missing config parameter', { status: 400 });
+        }
+  
+        return new Response(encodeBase64(finalString), {
+          headers: { 'content-type': 'application/json; charset=utf-8' }
+        });
+      } else if (url.pathname === '/favicon.ico') {
+        return Response.redirect('https://cravatar.cn/avatar/9240d78bbea4cf05fb04f2b86f22b18d?s=160&d=retro&r=g', 301)
       }
 
-      const finalString = finalProxyList.join('\n');
-
-      if (!finalString) {
-        return new Response('Missing config parameter', { status: 400 });
-      }
-
-      return new Response(encodeBase64(finalString), {
-        headers: { 'content-type': 'application/json; charset=utf-8' }
-      });
-    } else if (url.pathname === '/favicon.ico') {
-      return Response.redirect('https://cravatar.cn/avatar/9240d78bbea4cf05fb04f2b86f22b18d?s=160&d=retro&r=g', 301)
     }
-
+    
     return new Response('Not Found', { status: 404 });
   } catch (error) {
     console.error('Error processing request:', error);
